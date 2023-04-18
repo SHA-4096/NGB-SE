6209eaf428456ba2fd51a6bee2a460d282d59236
nullnullptr <xuzhanchaomail@163.com>
2023-04-18 23:22:47 +0800

    feat:实现了普通用户信息的修改

--- view/router.go
+++ view/router.go
@@ -10,6 +10,7 @@ func SetRouters(e *echo.Echo) {
 	e.POST("/user/register", controller.Register)
 	e.POST("/user/login", controller.Login)
 	e.GET("/user/delete/:Uid", controller.DeleteUser)
+	e.POST("/user/modify/:Uid", controller.ModifyUser)
 	e.GET("/user/admin/:AdminId/delete/:Uid", controller.AdminDeleteUser)
 	e.POST("/user/admin/:AdminId/modify/:Uid", controller.AdminModifyUser)
 }
