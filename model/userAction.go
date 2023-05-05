b91a317eb5b387247665898fbbc108d5203fbc21
nullnullptr <xuzhanchaomail@163.com>
2023-05-05 15:13:42 +0800

    update:开始写用户点赞记录

---
+++ model/userAction.go
@@ -0,0 +1,36 @@
+package model
+
+import "fmt"
+
+//
+//查询用户点赞的所有文章节点记录
+//
+func GetAllLikes(Uid string) ([]LikeAction, error) {
+	var actions []LikeAction
+	db.Find(&actions, "uid = ?", Uid)
+	return actions, nil
+}
+
+//
+//创建点赞记录
+//
+func CreateLike(Uid string, PassageId string) error {
+	var action LikeAction
+	err := db.Where("uid = ? AND passage_id = ?", Uid, PassageId).First(&action).Error
+	if err != nil {
+		//找不到记录
+		action.Uid = Uid
+		action.PassageId = PassageId
+		db.Save(&action)
+		return nil
+	}
+	return fmt.Errorf("这条点赞记录已经存在")
+}
+
+func DeleteLike(Uid string, PassageId string) error {
+	err := db.Where("uid = ? AND passage_id = ?", Uid, PassageId).Delete(&LikeAction{}).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
